c7290c481d35a754097cb9aa934d712cf4a824ba
“kiwanacollins” <collinzcalson@gmail.com>
2025-03-08 01:35:55 +0300

    refactor: split multiline text into separate lines for improved readability

--- src/components/InvoicePage.tsx
+++ src/components/InvoicePage.tsx
@@ -456,19 +456,18 @@ const InvoicePage: FC<Props> = ({ data, pdfMode, onChange }) => {
           
           <View className="flex" pdfMode={pdfMode}>
             <View className="flex-1" pdfMode={pdfMode}>
-              <Text className="bold fs-12" pdfMode={pdfMode}>
-                Plot 13, Kampala,{'\n'}Amadinda House
-              </Text>
+              <Text className="bold fs-12" pdfMode={pdfMode}>Plot 13, Kampala,</Text>
+              <Text className="bold fs-12" pdfMode={pdfMode}>Amadinda House</Text>
             </View>
             <View className="flex-1" pdfMode={pdfMode}>
-              <Text className="bold fs-12 center" pdfMode={pdfMode}>
-                Tel:{'\n'}+256 200 933 371{'\n'}+256 706 395 757{'\n'}256 772 560 792
-              </Text>
+              <Text className="bold fs-12 center" pdfMode={pdfMode}>Tel:</Text>
+              <Text className="bold fs-12 center" pdfMode={pdfMode}>+256 200 933 371</Text>
+              <Text className="bold fs-12 center" pdfMode={pdfMode}>+256 706 395 757</Text>
+              <Text className="bold fs-12 center" pdfMode={pdfMode}>256 772 560 792</Text>
             </View>
             <View className="flex-1" pdfMode={pdfMode}>
-              <Text className="bold fs-12 right" pdfMode={pdfMode}>
-                P.O.BOX 34338{'\n'}Kampala Uganda
-              </Text>
+              <Text className="bold fs-12 right" pdfMode={pdfMode}>P.O.BOX 34338</Text>
+              <Text className="bold fs-12 right" pdfMode={pdfMode}>Kampala Uganda</Text>
             </View>
           </View>
     
@@ -501,7 +500,7 @@ const InvoicePage: FC<Props> = ({ data, pdfMode, onChange }) => {
               className="mt-5 italic signature-name"
               pdfMode={pdfMode}
             >
-              {/* BT Repair */}
+              BT Repair
             </Text>
           </View>
         </View>

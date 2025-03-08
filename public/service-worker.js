3734b3fad176e2e5000a2c3aa805326f128d9777
“kiwanacollins” <collinzcalson@gmail.com>
2025-03-08 11:39:32 +0300

    refactor: update HTML meta tags and manifest for improved PWA support and branding

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-restricted-globals */
+
+// Cache name
+const CACHE_NAME = 'bt-repair-centre-v1';
+
+// Files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/logo512.png'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+          return null;
+        })
+      );
+    })
+  );
+});
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -24,36 +24,18 @@ type Config = {
 }
 
 export function register(config?: Config) {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL!, window.location.href)
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-      return
-    }
-
+  if ('serviceWorker' in navigator) {
+    const publicUrl = new URL(window.location.href);
+    
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
+      const swUrl = `/service-worker.js`;
 
-      if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl, config)
-
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
-        navigator.serviceWorker.ready.then(() => {
-          console.log(
-            'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit https://bit.ly/CRA-PWA',
-          )
-        })
+      if (process.env.NODE_ENV === 'production') {
+        registerValidSW(swUrl, config);
       } else {
-        // Is not localhost. Just register service worker
-        registerValidSW(swUrl, config)
+        checkValidServiceWorker(swUrl, config);
       }
-    })
+    });
   }
 }
 

5bf26c9bdc2002d65d8e6081361738c83c811c00
kiwanacollins <collinzcalson@gmail.com>
2026-02-22 20:11:34 +0300

    Add responsive styles for mobile and very small screens
    - Introduced a new SCSS file for mobile responsiveness with breakpoints for small phones and tablets.
    - Adjusted layout properties for the app container, invoice wrapper, and various elements to ensure proper display on smaller screens.
    - Implemented font size adjustments and margin reductions for better vertical space management.
    - Ensured that table columns and footer elements stack appropriately on very small screens.
    - Added styles for download button repositioning and row remove button visibility on touch devices.
    - Included specific width overrides for percentage-based columns to maintain usability on mobile devices.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9e9p5ceg618"
+    "revision": "0.ep1a7u3od2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
